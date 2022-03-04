6b90c800cdd3bb26f0fe295b8c9a7254fa0da647
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-04 20:42:23 +0500

    add tree node pretty printer

--- src/main/kotlin/com/bakigoal/easy/TreeNode.kt
+++ src/main/kotlin/com/bakigoal/easy/TreeNode.kt
@@ -1,23 +1,28 @@
 package com.bakigoal.easy
 
-data class TreeNode(val value: Int, val left: TreeNode? = null, val right: TreeNode? = null) {
-    fun prettyPrint(): String = prettyPrintTree(this, "", true)
-}
+data class TreeNode(val value: Int, val left: TreeNode? = null, val right: TreeNode? = null)
+
+fun TreeNode.prettyPrint() = prettyPrintTree(this, "", true)
 
 fun prettyPrintTree(node: TreeNode?, prefix: String, isLeft: Boolean): String {
     if (node == null) {
         return "Empty tree"
     }
     val sb = StringBuilder()
+
+    // print right
     if (node.right != null) {
         sb.append(prettyPrintTree(node.right, prefix + if (isLeft) "│   " else "    ", false))
     }
-    val printNode = prefix + (if (isLeft) "└── " else "┌── ") + node.value
-    sb.append(printNode)
-    sb.append("\n")
+
+    // print value
+    sb.append(prefix + (if (isLeft) "└── " else "┌── ") + node.value).append("\n")
+
+    // print left
     if (node.left != null) {
         sb.append(prettyPrintTree(node.left, prefix + if (isLeft) "    " else "│   ", true))
     }
+
     return sb.toString()
 }
 
