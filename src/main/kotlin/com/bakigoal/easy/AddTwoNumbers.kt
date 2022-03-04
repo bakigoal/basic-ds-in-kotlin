102be2eb1d283b173f74ec81e7d9377ecaafc2d8
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-04 14:37:48 +0500

    add two numbers refactoring

--- src/main/kotlin/com/bakigoal/easy/AddTwoNumbers.kt
+++ src/main/kotlin/com/bakigoal/easy/AddTwoNumbers.kt
@@ -20,32 +20,23 @@ data class ListNode(var `val`: Int, var next: ListNode? = null) {
 fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
     var node1 = l1
     var node2 = l2
-    var memory = 0
-
-    var value1 = node1?.`val` ?: 0
-    var value2 = node2?.`val` ?: 0
-    var sum = value1 + value2 + memory
-    memory = sum / 10
-    var curValue = sum % 10
-    val result = ListNode(curValue)
+    var inMemory = 0
+    val result = ListNode(-1)
     var currentNode = result
 
-    while (node1?.next != null || node2?.next != null) {
+    while (node1 != null || node2 != null) {
+        val sum = (node1?.`val` ?: 0) + (node2?.`val` ?: 0) + inMemory
+        inMemory = sum / 10
+        currentNode.next = ListNode(sum % 10)
+        currentNode = currentNode.next!!
+
         node1 = node1?.next
         node2 = node2?.next
-
-        value1 = node1?.`val` ?: 0
-        value2 = node2?.`val` ?: 0
-        sum = value1 + value2 + memory
-        memory = sum / 10
-        curValue = sum % 10
-        currentNode.next = ListNode(curValue)
-        currentNode = currentNode.next!!
     }
 
-    if (memory > 0) {
-        currentNode.next = ListNode(memory)
+    if (inMemory > 0) {
+        currentNode.next = ListNode(inMemory)
     }
 
-    return result
+    return result.next
 }
